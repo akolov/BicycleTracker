ce4b8efb85e745f59326f8888a9e2fbaf9aa9104
Alexander Kolov <me@alexkolov.com>
2015-03-10 20:19:51 +0100

    Initial Commit

---
+++ BicycleTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BicycleTracker
+//
+//  Created by Alexander Kolov on 3/10/15.
+//  Copyright (c) 2015 Alexander Kolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
